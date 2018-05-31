81ddd81a02ed89aa14776ef4768306e890532290
Dan Garland <dan@wegotcoders.com>
2018-05-31 13:20:58 +0100

    Fixing index redirect and using the existing token from the session

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -3,8 +3,9 @@ class RepoController < ApplicationController
   before_action :ensure_access_token, :except => :oauth2_callback
 
   def index
-    repos = oauth2_client.get('https://api.github.com/user/repo').parsed
-    @names = repos.map {|repo| repo[:name] }
+    authorised_client = OAuth2::AccessToken.new(oauth2_client, session[:access_token])
+    repos = authorised_client.get('https://api.github.com/user/repos').parsed
+    @names = repos.map {|repo| repo['name'] }
   end
 
   # TODO: This ought to be in a AuthorisationController or somesuch...
@@ -15,6 +16,6 @@ class RepoController < ApplicationController
 
     # Remember the access token
     session[:access_token] = token.token
-    redirect_to :index
+    redirect_to repo_index_path
   end
 end
